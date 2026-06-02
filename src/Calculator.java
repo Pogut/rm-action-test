446c8ca15f4d0b2f411ccf3dcef6a55b3bf6296f
Parsa Hejazi <p.hejazi2014@gmail.com>
2026-06-02 11:51:22 -0400

    cleanup

--- src/Calculator.java
+++
@@ -1,9 +0,0 @@
-public class Calculator {
-    public int processOrder(int price, int quantity, double taxRate) {
-        int subtotal = price * quantity;
-        double tax = subtotal * taxRate;
-        int total = (int)(subtotal + tax);
-        System.out.println("Order total: " + total);
-        return total;
-    }
-}
